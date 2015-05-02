383b42ff117a9ffdb0cccc8182154b8f5e4e7a19
Rajith Radhakrishnan <radrajith@gmail.com>
2015-05-01 21:10:23 -0400

    deleted Control class, and matrix.
    reverted back to the initial stages with just the windows full of just
    buttons, due to exception error in previous version

--- src/minesweeper/Built_mines.java
+++ src/minesweeper/Built_mines.java
@@ -3,67 +3,40 @@ package minesweeper;
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.input.*;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 import javafx.scene.control.Control;
 import javafx.scene.shape.Rectangle;
-import javafx.event.Event;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 public class Built_mines extends Application{
 	final static int screenSize_x = 368;
 	final static int screenSize_y = 450;
-	private double button_width = 0.0;
-	private double button_height = 0.0;
+	private double button_width;
+	private double button_height;
 	private int y_mem= 0;
 	private Button[] button = new Button[(screenSize_x/19*screenSize_y/30 )+3];
-	
+	Group root = new Group();
 	public void start(Stage stage){
-		//createButtons();		
-		GridPane grid = new GridPane();	
-		Group root = new Group();
-		//root.getChildren().addAll(getSizeOfCell(button_width, button_height));
-		Control[][] matrix = new Control[(int)(screenSize_x/16)][(int)(screenSize_y/18)];
-		for(int m=0; m<=16;m++){
-		    for(int n=0; n<=30; n++){
-				root.getChildren().add(matrix[m][n]);
-				int pos = (m*(screenSize_y/18))+n;
-				button[pos] = new Button(" ");
-				grid.add(button[pos] , m, n);
-				mouseClick(pos);
-			}
-		}
+		createButtons();	
+		getGrid();
 		Scene scene = new Scene(root, screenSize_x, screenSize_y);
 		stage.setScene(scene);
 		stage.show();
 		stage.setTitle("MINESWEEPER GAME");
 		
 	}	
-	
-	private void mouseClick(int pos){
-		button[pos].setOnMouseClicked(new EventHandler<MouseEvent>(){public void handle(MouseEvent event) {System.out.println("Hello World!");}});
-
-	//	button[pos].setOnMouseClicked(new EventHandler<MouseEvent>(){
-    //        public void handle(MouseEvent event) {
-    //        	if(event.getButton().equals(event.isPrimaryButtonDown())){
-    //        	     System.out.println("Hello World!");
-    //        	}
-    //       }
-	//	}
-	}
-	
 	private void createButtons() {
 		for(int i = 0; i<button.length; i++){
 			button[i] = new Button("  ");
 		}
 		
 	}
-	private Pane getGrid(){
+	private void getGrid(){
 		int i=0, y = 0, x=0;
 		GridPane grid = new GridPane();
 		
@@ -76,12 +49,12 @@ public class Built_mines extends Application{
 				i++;
 				x++;
 		}
-		y_mem=y;
-		button_width = button[i].getWidth();
-		button_height = button[i].getHeight();
-		System.out.println(grid.getHeight());
-		System.out.println(button_height);
-		return grid;
+		//y_mem=y;
+		//button_width = button[i].getWidth();
+		//button_height = button[i].getHeight();
+		//System.out.println(grid.getHeight());
+		//System.out.println(button_height);
+		root.getChildren().add(grid);
 	}
 
 		        // label.setGraphic(new ImageView(getClass().getResource("/images/Folder-icon.png").toExternalForm()));
